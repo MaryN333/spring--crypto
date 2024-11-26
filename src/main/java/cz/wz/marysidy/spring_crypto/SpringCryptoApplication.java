17d787daadf40d68902b727dd5b1b7b8cafd3072
MaryN333 <sidyakina@gmail.com>
2024-11-26 13:03:44 +0100

    Initial commit

---
+++ src/main/java/cz/wz/marysidy/spring_crypto/SpringCryptoApplication.java
@@ -0,0 +1,13 @@
+package cz.wz.marysidy.spring_crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCryptoApplication.class, args);
+	}
+
+}
